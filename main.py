eb6c37fd4bc952f180825cd58095ab46b63a5686
McCancer <mkunzer@DESKTOP-VOGQ053>
2025-01-23 01:00:51 -0600

    Ch2L2 blackscreen appearing

--- main.py
+++ main.py
@@ -1,7 +1,19 @@
 import pygame
+import os
+from constants import *
 
 def main():
-    print("Starting asteroids!")
+    pygame.init()
+    running = True
+    screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    while(running):
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                return
+        screen.fill((0,0,0))
+        pygame.display.flip()
+
 
 if __name__ == "__main__":
+    os.environ["SDL_AUDIODRIVER"] = "dsp"
     main()
