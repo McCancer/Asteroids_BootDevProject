71903ffd8a7a363868ae22acefa416bec3bce0d2
McCancer <mkunzer@DESKTOP-VOGQ053>
2025-01-23 02:05:20 -0600

    Ch2L5 Drawing Player

--- main.py
+++ main.py
@@ -1,19 +1,24 @@
 import pygame
 import os
+from player import Player
 from constants import *
 
 def main():
     pygame.init()
+    gameClock = pygame.time.Clock()
+    dt = 0
     running = True
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    player = Player((SCREEN_WIDTH/2), (SCREEN_HEIGHT/2))
     while(running):
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
         screen.fill((0,0,0))
+        player.draw(screen)
         pygame.display.flip()
+        dt = gameClock.tick(60) / 1000
 
 
 if __name__ == "__main__":
-    os.environ["SDL_AUDIODRIVER"] = "dsp"
     main()
