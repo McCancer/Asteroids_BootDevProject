c0d5060dfc2ff52d1b0438ad30d2bc7fd04f4deb
McCancer <mkunzer@DESKTOP-VOGQ053>
2025-01-24 23:52:47 -0600

    CH3L4: Adding shooting and shooting limits

--- main.py
+++ main.py
@@ -4,6 +4,7 @@ from constants import *
 from player import Player
 from asteroid import Asteroid
 from asteroidfield import AsteroidField
+from shot import Shot
 
 def main():
     pygame.init()
@@ -14,8 +15,11 @@ def main():
     updatable = pygame.sprite.Group()
     drawable = pygame.sprite.Group()
     asteroids = pygame.sprite.Group()
+    shots = pygame.sprite.Group()
+
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     Player.containers = (updatable, drawable)
+    Shot.containers = (updatable, drawable)
     Asteroid.containers = (asteroids, updatable, drawable)
     AsteroidField.containers = (updatable)
 
--- player.py
+++ player.py
@@ -1,11 +1,14 @@
 from circleshape import *
 import pygame
 from constants import *
+from shot import Shot
 
 class Player(CircleShape):
+
     def __init__(self,x,y):
         super().__init__(x, y, PLAYER_RADIUS)
         self.rotation = 0
+        self.timer = 0
 
 
     def triangle(self):
@@ -28,6 +31,12 @@ class Player(CircleShape):
         #Multiply the Vector by the player speed and delta time and add it to our position 
         self.position += forward * PLAYER_SPEED * dt
 
+    def shoot(self):
+        shot = Shot(self.position.x, self.position.y)
+        shot_velocity = pygame.Vector2(0,1).rotate(self.rotation)
+        shot.velocity = shot_velocity * PLAYER_SHOT_SPEED
+        self.timer = PLAYER_SHOT_COOLDOWN
+
     def update(self, dt):
         keys = pygame.key.get_pressed()
         if keys[pygame.K_a]:
@@ -38,4 +47,8 @@ class Player(CircleShape):
             self.move(dt)
         if keys[pygame.K_s]:
             self.move(dt*-1)
+        if(keys[pygame.K_SPACE] and not self.timer > 0):
+            self.shoot()
+        self.timer -= dt
+        
         
